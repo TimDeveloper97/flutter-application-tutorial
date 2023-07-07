d23e73ff55f20dbb2db1613e878d307ddeb72313
DinhDuyAnh <duyanh.dinh@veriserve-vietnam.com>
2023-07-07 08:46:56 +0700

    android platform

--- custom_renderer_platform_battery_level/android/app/src/main/kotlin/com/example/custom_renderer_platform_battery_level/MainActivity.kt
+++ custom_renderer_platform_battery_level/android/app/src/main/kotlin/com/example/custom_renderer_platform_battery_level/MainActivity.kt
@@ -3,5 +3,24 @@ package com.example.custom_renderer_platform_battery_level
 import io.flutter.embedding.android.FlutterActivity
 
 class MainActivity: FlutterActivity() {
-    
+    private val CHANNEL = "samples.flutter.dev/battery";
+
+    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+        super.configureFlutterEngine(flutterEngine);
+        MethodChannel(flutterEngine.dartExecutor.binaryMessenger, CHANNEL).setMethodCallHandler {
+            call, result -> 
+
+            if (call.method == "getBatteryLevel") {
+            val batteryLevel = getBatteryLevel()
+
+            if (batteryLevel != -1) {
+                result.success(batteryLevel)
+            } else {
+                result.error("UNAVAILABLE", "Battery level not available.", null)
+            }
+            } else {
+                result.notImplemented()
+            }
+        }
+    }
 }
